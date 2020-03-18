7cf7a016d2281f34c6d2346e364ff09971e3428b
Trismegiste <flo@trismegiste.net>
2020-03-18 22:11:43 +0100

    swaping to json storage

--- src/Repository/DocumentFactory.php
+++ src/Repository/DocumentFactory.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\GraphSpeech\Document;
+use App\Entity\GraphSpeech\Sentence;
 use IteratorAggregate;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
@@ -26,17 +27,40 @@ class DocumentFactory {
 
     public function list(): IteratorAggregate {
         $iter = new Finder();
-        $iter->in($this->basedir)->name("*.doc")->files();
+        $iter->in($this->basedir)->name("*.json")->files();
 
         return $iter;
     }
 
     public function save(Document $doc): void {
-        $this->filesystem->dumpFile($this->basedir . $doc->getTitle() . '.doc', serialize($doc));
+        $flatten = [
+            'title' => $doc->getTitle(),
+            'description' => $doc->getDescription()
+        ];
+        foreach ($doc as $vertex) {
+            $flatten['vertex'][] = [
+                'key' => $vertex->getKey(),
+                'category' => $vertex->getCategory(),
+                'content' => $vertex->getContent()
+            ];
+        }
+
+        $this->filesystem->dumpFile($this->basedir . $doc->getTitle() . '.json', json_encode($flatten));
     }
 
     public function load(string $title): Document {
-        return unserialize(file_get_contents($this->basedir . $title . '.doc'));
+        $flatten = json_decode(file_get_contents($this->basedir . $title . '.json'));
+
+        $doc = $this->create();
+        $doc->setTitle($flatten->title);
+        $doc->setDescription($flatten->description);
+        foreach ($flatten->vertex as $vertex) {
+            $stc = new Sentence($doc, $vertex->key);
+            $stc->setCategory($vertex->category);
+            $stc->setContent($vertex->content);
+        }
+
+        return $doc;
     }
 
 }
