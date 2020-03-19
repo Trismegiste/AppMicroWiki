0e8e3cfee4a775c8e09e66467d9dfc5be874c4fb
Trismegiste <flo@trismegiste.net>
2020-03-19 12:54:35 +0100

    sentencetype fully tested

--- src/Form/SentenceType.php
+++ src/Form/SentenceType.php
@@ -3,35 +3,85 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Trismegiste\MicroWiki\Document;
 use Trismegiste\MicroWiki\Sentence;
 
 /**
  * Description of SentenceType
  */
-class SentenceType extends AbstractType {
+class SentenceType extends AbstractType implements DataMapperInterface {
+
+    protected $parentDocument;
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
+        $this->parentDocument = $options['document'];
+
         $builder
                 ->add('key', TextType::class, ['label' => 'Name'])
                 ->add('category', TextType::class)
                 ->add('content', TextareaType::class)
-                ->add('save', SubmitType::class);
+                ->add('save', SubmitType::class)
+                ->setDataMapper($this);
     }
 
     public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setRequired(['document']);
         $resolver->setDefaults([
+            'document' => null,
             'data_class' => Sentence::class,
             'empty_data' => function(FormInterface $form) {
                 $obj = new Sentence($form->get('key')->getData());
                 return $obj;
             }
         ]);
+        $resolver->setAllowedTypes('document', Document::class);
+    }
+
+    public function mapDataToForms($viewData, $forms) {
+        // there is no data yet, so nothing to prepopulate
+        if (null === $viewData) {
+            return;
+        }
+
+        // invalid data type
+        if (!$viewData instanceof Sentence) {
+            throw new UnexpectedTypeException($viewData, Sentence::class);
+        }
+
+        /** @var FormInterface[] $forms */
+        $forms = iterator_to_array($forms);
+
+        // initialize form field values
+        $forms['key']->setData($viewData->getKey());
+        $forms['category']->setData($viewData->getCategory());
+        $forms['content']->setData($viewData->getContent());
+    }
+
+    public function mapFormsToData($forms, &$viewData) {
+        // invalid data type
+        if (!$viewData instanceof Sentence) {
+            throw new UnexpectedTypeException($viewData, Sentence::class);
+        }
+        /** @var FormInterface[] $forms */
+        $forms = iterator_to_array($forms);
+        $viewData->setCategory($forms['category']->getData());
+        $viewData->setContent($forms['content']->getData());
+
+        if (!array_key_exists($viewData->getKey(), $this->parentDocument)) {
+            $this->parentDocument[] = $viewData;
+        }
+
+        if ($viewData->getKey() !== $forms['key']->getData()) {
+            $viewData->renameKey($this->parentDocument, $forms['key']->getData());
+        }
     }
 
 }
--- tests/Form/SentenceTypeTest.php
+++ tests/Form/SentenceTypeTest.php
@@ -2,12 +2,14 @@
 
 use App\Form\SentenceType;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Trismegiste\MicroWiki\Document;
 use Trismegiste\MicroWiki\Sentence;
 
 class SentenceTypeTest extends TypeTestCase {
 
     public function testEmptyData() {
-        $form = $this->factory->create(SentenceType::class);
+        $doc = new Document();
+        $form = $this->factory->create(SentenceType::class, null, ['document' => $doc]);
         $formData = [
             'key' => 'HAL9000',
             'category' => "evil computer",
@@ -18,20 +20,26 @@ class SentenceTypeTest extends TypeTestCase {
         $stc = $form->getData();
         $this->assertInstanceOf(Sentence::class, $stc);
         $this->assertEquals('HAL9000', $stc->getKey());
+        $this->assertEquals('evil computer', $stc->getCategory());
+        $this->assertArrayHasKey('HAL9000', $doc);
     }
 
     public function testEdit() {
+        $doc = new Document();
         $stc = new Sentence("HAL9000");
-        $form = $this->factory->create(SentenceType::class, $stc);
+        $form = $this->factory->create(SentenceType::class, $stc, ['document' => $doc]);
 
         $formData = [
+            'key' => 'IBM8000',
             'category' => 'chess master',
             'content' => "Murderer of [[Frank Poole]]"
         ];
         $form->submit($formData);
         $this->assertTrue($form->isSynchronized());
 
+        $this->assertEquals('IBM8000', $stc->getKey());
         $this->assertEquals('chess master', $stc->getCategory());
+        $this->assertArrayHasKey('IBM8000', $doc);
     }
 
 }
