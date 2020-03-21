24e275b64698855dd6dde1ffaf4754e1f011c21b
Trismegiste <flo@trismegiste.net>
2020-03-21 23:22:12 +0100

    restart to default security

--- src/Controller/Landing.php
+++ src/Controller/Landing.php
@@ -18,20 +18,4 @@ class Landing extends AbstractController {
         return $this->redirectToRoute('app_documentcrud_list');
     }
 
-    /**
-     * @Route("/login", name="login", methods={"GET", "POST"})
-     */
-    public function login(AuthenticationUtils $authenticationUtils) {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', [
-                    'last_username' => $lastUsername,
-                    'error' => $error,
-        ]);
-    }
-
 }
