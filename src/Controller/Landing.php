b2399f8cb8ab7b843aba311904455311311ebbaa
Trismegiste <flo@trismegiste.net>
2020-03-21 22:45:55 +0100

    revert before make:user

--- src/Controller/Landing.php
+++ src/Controller/Landing.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Landing page
@@ -15,6 +16,22 @@ class Landing extends AbstractController {
      */
     public function home() {
         return $this->redirectToRoute('app_documentcrud_list');
+    }
+
+    /**
+     * @Route("/login", name="login", methods={"GET", "POST"})
+     */
+    public function login(AuthenticationUtils $authenticationUtils) {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+                    'last_username' => $lastUsername,
+                    'error' => $error,
+        ]);
     }
 
 }
