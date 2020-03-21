d8cf61449c949099a98da7c670d58c825771049d
Trismegiste <flo@trismegiste.net>
2020-03-21 15:52:38 +0100

    create a console command for security

---
+++ src/Command/CreateUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Description of CreateUserCommand
+ */
+class CreateUser extends Command {
+
+    protected static $defaultName = 'app:create-user';
+
+    protected function configure() {
+        $this->setDescription('Create a new admin user')
+                ->addArgument('user', InputArgument::REQUIRED, 'username');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+        $io = new SymfonyStyle($input, $output);
+        $username = $input->getArgument('user');
+        $io->title($this->getDescription() . ' : ' . $username);
+
+
+        return 0;
+    }
+
+}
