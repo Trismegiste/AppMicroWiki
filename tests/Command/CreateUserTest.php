3a3679492340ac239cdab5a11b5d3d0256383728
Trismegiste <flo@trismegiste.net>
2020-03-22 06:43:47 +0100

    testing create user cmd

---
+++ tests/Command/CreateUserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class CreateUserTest extends KernelTestCase {
+
+    public function testExecute() {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:create-user');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'user' => 'Oshii',
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Oshii', $output);
+    }
+
+}
