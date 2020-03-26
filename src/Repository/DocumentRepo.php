83d02992a5396288dcde826f5c399bda96af29c2
Trismegiste <flo@trismegiste.net>
2020-03-26 18:27:17 +0100

    transform Document Repo into an Adapter pattern

--- src/Repository/DocumentRepo.php
+++ src/Repository/DocumentRepo.php
@@ -6,65 +6,44 @@
 
 namespace App\Repository;
 
+use Iterator;
 use MongoDB\BSON\ObjectId;
 use MongoDB\Driver\BulkWrite;
-use MongoDB\Driver\Manager;
 use MongoDB\Driver\Query;
 use Trismegiste\MicroWiki\Document;
+use Trismegiste\Toolbox\Iterator\ClosureDecorator;
+use Trismegiste\Toolbox\MongoDb\Repository;
 
 /**
- * Repository of Document
+ * Adapter of Document
  */
 class DocumentRepo {
 
-    const collectionName = 'wiki';
+    protected $repository;
 
-    protected $mongo;
-    protected $dbName;
-
-    public function __construct(Manager $man, string $database) {
-        $this->mongo = $man;
-        $this->dbName = $database;
+    public function __construct(Repository $documentRepo) {
+        $this->repository = $documentRepo;
     }
 
-    private function getNamespace() {
-        return $this->dbName . '.' . self::collectionName;
-    }
+    public function list(): Iterator {
+        $iter = $this->repository->search();
 
-    public function list(): array {
-        $cursor = $this->mongo->executeQuery($this->getNamespace(), new Query([]));
-
-        $listing = [];
-        foreach ($cursor as $docu) {
-            $listing[] = (object) [
+        return new ClosureDecorator($iter, function(Document $docu) {
+            return (object) [
                         'pk' => $docu->getPk(),
                         'title' => $docu->getTitle(),
                         'description' => $docu->getDescription(),
                         'vertex' => count($docu)
             ];
-        }
-
-        return $listing;
+        });
     }
 
     public function save(Document $doc): void {
-        $bulk = new BulkWrite();
-
-        if ($doc->isNew()) {
-            $id = $bulk->insert($doc);
-            $doc->setPk($id);
-        } else {
-            $bulk->update(['_id' => $doc->getPk()], $doc);
-        }
-
-        $this->mongo->executeBulkWrite($this->getNamespace(), $bulk);
+        $this->repository->save($doc);
     }
 
     public function load(string $pk): Document {
-        $cursor = $this->mongo->executeQuery($this->getNamespace(), new Query(['_id' => new ObjectId($pk)]));
-        $rows = iterator_to_array($cursor);
-
-        return $rows[0];
+        return $this->repository->load($pk);
     }
 
 }
